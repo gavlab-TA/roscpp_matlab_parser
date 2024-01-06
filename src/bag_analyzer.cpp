6a25c5e20be379ad01cd40c1b7b0da13ccb1e073
Kyle Thompson <kat0028@auburn.edu>
2024-01-06 10:14:23 -0600

    added fix that ensures first topic is pulled

--- src/bag_analyzer.cpp
+++ src/bag_analyzer.cpp
@@ -259,6 +259,34 @@ int main()
             if (prefix_check == start_parse_prefix)
             {
                 ready = true;
+                temp.erase(0, prefix_check.length() + 1);
+                if (temp.length() != 0)
+                {
+                    line_data.clear();
+                    line_data = split(temp, '|');
+                    std::string count = line_data.at(2).erase(0, 8);
+                    if (count.at(0) != '0')
+                    {
+
+                        std::string name = line_data.at(0).erase(0, 8);
+                        name.erase(name.length() - 1, name.length());
+                        topic_names.push_back(name);
+                        std::string type = line_data.at(1).erase(0, 7);
+                        type.erase(type.length() - 1, type.length());
+                        bool save_message = !checkForString(message_names, type);
+                        raw_message_name = type;
+                        if (save_message)
+                        {
+                            message_names.push_back(type);
+                        }
+                        type = formatMessageTypes(type);
+                        if (save_message)
+                        {
+                            message_types.push_back(type);
+                        }
+                        topic_data.push_back(name + "#" + type + "#" + raw_message_name);
+                    }
+                }
             }
         }
         else
